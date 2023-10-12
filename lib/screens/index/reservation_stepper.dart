c1cc7d3619534dacd620edf50e5d77efb2fb8d01
lum23 <cassylum23@gmail.com>
2023-10-12 14:34:43 +0800

    working on stepper

--- lib/screens/index/reservation_stepper.dart
+++ lib/screens/index/reservation_stepper.dart
@@ -13,8 +13,27 @@ class _ReservationStepperState extends State<ReservationStepper> {
       $Color1_background = const Color(0xff013440),
       $Color2_accent = const Color(0xfffbb245),
       $Color3_text = const Color(0xfff8f8f8),
-      $Color4_gray = const Color(0xff9e9e9e),
-      $temp_var = 9.6;
+      $Color4_gray = const Color(0xff9e9e9e);
+
+  int _currentStepIndex = 0;
+
+  List<Step> steps = [
+    const Step(
+      title: Text(''),
+      content: Text('Content for Step 1'),
+      isActive: true,
+    ),
+    const Step(
+      title: Text(''),
+      content: Text('Content for Step 2'),
+      isActive: false,
+    ),
+    const Step(
+      title: Text(''),
+      content: Text('Content for Step 3'),
+      isActive: false,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -24,21 +43,47 @@ class _ReservationStepperState extends State<ReservationStepper> {
     return Material(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('HOME'),
+          title: const Text('Chosda'),
           backgroundColor: $Color1_background,
           toolbarHeight: 80,
           elevation: 0.0,
           actions: <Widget>[
             IconButton(
-                onPressed: () => $temp_var,
-                icon: const Icon(Icons.account_circle))
+              onPressed: () => $ScreenHeight,
+              icon: const Icon(Icons.account_circle),
+            ),
           ],
-          // Adjust the height to your desired value
         ),
         body: SafeArea(
-          child: Stepper(
-            steps: steps
-          )
+          child: Column(
+            children: [
+              Text('asdada'),
+              Stepper(
+                currentStep: _currentStepIndex,
+                type: StepperType.horizontal,
+                onStepCancel: () {
+                  if (_currentStepIndex > 0) {
+                    setState(() {
+                      _currentStepIndex -= 1;
+                    });
+                  }
+                },
+                onStepContinue: () {
+                  if (_currentStepIndex < steps.length - 1) {
+                    setState(() {
+                      _currentStepIndex += 1;
+                    });
+                  }
+                },
+                onStepTapped: (int index) {
+                  setState(() {
+                    _currentStepIndex = index;
+                  });
+                },
+                steps: steps,
+              ),
+            ],
+          ),
         ),
       ),
     );
